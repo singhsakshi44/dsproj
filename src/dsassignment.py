29a76ff875b1c5fb2491d89bd04bc185a87e1de8
Sakshi Singh <Sakshi.Singh@jktech.com>
2026-04-06 14:33:29 +0530

    second version of program. its having basic methods, now will modify per req

--- src/dsassignment.py
+++ src/dsassignment.py
@@ -19,23 +19,21 @@ def create_col_mapping(mapping):
         for col in variations:
             revcolumn_mapping[col.lower().strip()] = standard
 
-    print(revcolumn_mapping)
     return revcolumn_mapping
 
 def standardrize_columns(tdf,revcol_mapping):
     new_columns = []
-    new_colname = ""
+    
     
     for col in tdf.columns:
         clean_col = col.lower().strip()
 
+        new_name = ""
         if clean_col in revcol_mapping:
-            new_colname = (revcol_mapping[clean_col])
-        else:
-            new_colname = clean_col
+            new_name = (revcol_mapping[clean_col])
 
-        if new_colname not in new_columns:
-            new_columns.append(new_colname)
+        if new_name != "" and new_name not in new_columns:
+            new_columns.append(new_name)
         else:
             new_columns.append(clean_col)  # keep as is
         
@@ -49,9 +47,7 @@ def read_dir(rootpath):
 
     # OS.walk is fastest than pathlib or glob
     for dirpath,dirname,filesname in os.walk(rootpath):
-        # print(f"1= {dirpath} 2= {dirname} 3= {filename}")
-        # filepathlist = [file for file in filesname if ".csv" in file]
-
+       
         for file in filesname:
             if file.endswith(".csv"):
                 filepathlist.append(os.path.join(dirpath, file))
@@ -63,10 +59,11 @@ def check_file_columns(filenamelist):
     
     all_columns = []
     summ_data = []
-    # Extract all column sets
     
+    # Extract all column sets    
     all_columns = [set(pd.read_csv(f, nrows=0).columns) for f in filenamelist]
     # all_columns.append(pd.read_csv(f, nrows=0).columns for f in filenamelist)
+
     for f in filenamelist:
         tdf = pd.read_csv(f, nrows=0)
         cols = ", ".join(tdf.columns)
@@ -78,10 +75,10 @@ def check_file_columns(filenamelist):
     
     return summ_data
    
-    # # Find intersection across all sets
+    # Find intersection across all sets
     # common_to_all = reduce(set.intersection, all_columns)
     # print(common_to_all)
-    # print(len(common_to_all))
+    # print(len(common_to_all)
     # print(f"Columns in every file: {common_to_all}")
 
 
@@ -95,6 +92,12 @@ def read_files(filenamelist,rev_col_mapping):
                             low_memory=False
         )
         tmpdf = standardrize_columns(tmpdf,rev_col_mapping)
+   
+        if 'uniq_id' in tmpdf.columns:
+            tmpdf['new_uniq_id'] = tmpdf['uniq_id'].replace('', pd.NA).fillna(tmpdf.get('fingerprint'))
+        else:
+            tmpdf['new_uniq_id'] = tmpdf.get('fingerprint')
+
         tmpdf['source_file'] = os.path.basename(file)
         datafrms.append(tmpdf)
 
@@ -106,19 +109,15 @@ def data_cleaning(ipdf):
     # print(ipdf.info())  
 
     # remove rows with null in key cols
-    ipdf.dropna(subset=['age','sex'],inplace=True)
+    ipdf.dropna(subset=['new_uniq_id'],inplace=True)
 
     #Remove any duplicates
     ipdf.drop_duplicates(inplace=True)    
 
-    # fill data in empty cell with average value
-    chol_meanval = int(ipdf["chol"].mean())    
-    ipdf.fillna({"chol":chol_meanval},inplace=True)
-
-    # Clean wrong data-Either renove it ot fix data like define rule based on columns
-    for x in ipdf.index:
-        if ipdf.loc[x,"cp"] > 3:
-            ipdf.loc[x,'cp'] = 3
+    # Clean wrong data-Either renove it or fix data like define rule based on columns
+    # for x in ipdf.index:
+    #     if ipdf.loc[x,"cp"] > 3:
+    #         ipdf.loc[x,'cp'] = 3
     
     return ipdf
 
@@ -147,32 +146,31 @@ def data_analysis(ipfdf,rec_count,file_count,):
 
 def mainblk(rootpath,summaryfile,outputfile):
     revcolumn_mapping = create_col_mapping(column_mapping)
+
     files_to_scan = read_dir(rootpath)
+
     cols_data = check_file_columns(files_to_scan)
-    mergeddf = read_files(files_to_scan,column_mapping)
-    merged_cnt = len(mergeddf)
-    #print(merged_cnt)
-    # print(mergeddf.info()) 
-
     my_df = pd.DataFrame(cols_data)
-
     # Save to CSV
-    my_df.to_csv(outputfile, index=False)
+    my_df.to_csv("C:/WorkSakshi/Python/data/dumpOutput/AllColsSumm.csv", index=False)
+
+    # unified Consolidated Data
+    mergeddf = read_files(files_to_scan,revcolumn_mapping)
+    merged_cnt = len(mergeddf)
+    
+    print(mergeddf.info()) 
+
+    finaldf = data_cleaning(mergeddf)
+    summary = data_analysis(finaldf,merged_cnt,len(files_to_scan))
+     # file containing all processed data
+    finaldf.to_csv(outputfile)
 
     with open(summaryfile, "w") as f:
-        mergeddf.info(buf=f)
-    
-    # cleanmasterdf = data_cleaning(mergeddf)
-    # summary = data_analysis(cleanmasterdf,merged_cnt,len(files_to_scan))
-    #  # file containing all processed data
-    # cleanmasterdf.to_csv(outputfile)
-
-    # with open(summaryfile, "w") as f:
-    #     for key, value in summary.items():
-    #         f.write(f"{key}, {value}\n")
+        for key, value in summary.items():
+            f.write(f"{key}, {value}\n")
 
 
-
+# Set path and file names
 rootfolderpath = "C:/WorkSakshi/Python/data/1"
 summaryfilename = "C:/WorkSakshi/Python/data/dumpOutput/summ.csv"
 consolidatedfilename = "C:/WorkSakshi/Python/data/dumpOutput/AllData.csv"
