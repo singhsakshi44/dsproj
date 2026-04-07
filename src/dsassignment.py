cc3616e9bbde4f7b35a143810201efdc8d7016f4
Sakshi Singh <Sakshi.Singh@jktech.com>
2026-04-07 09:25:01 +0530

    Changes per req given by reviewer

--- src/dsassignment.py
+++ src/dsassignment.py
@@ -1,36 +1,31 @@
 
 import os
 import pandas as pd
-from functools import reduce
+import json
 
 
 column_mapping = {
-    "uniq_id": ["src_sys_id", "sfdcid", "erp_src_sys_id"],
-    "global_id": ["erp_global", "sfdc_global"],
-    "similar_score": ["sim_s"]
+    "finalscore": ["completescore", "totalscore", "uniquenessscore"]
 }
 
+revcolumn_mapping = {}
+
+for standard, variations in column_mapping.items():
+    for col in variations:
+        revcolumn_mapping[col.lower().strip()] = standard
+
 master_df = pd.DataFrame()
+ 
 
-def create_col_mapping(mapping):
-    revcolumn_mapping = {}
-
-    for standard, variations in mapping.items():
-        for col in variations:
-            revcolumn_mapping[col.lower().strip()] = standard
-
-    return revcolumn_mapping
-
-def standardrize_columns(tdf,revcol_mapping):
+def standardrize_columns(tdf):
     new_columns = []
-    
-    
+        
     for col in tdf.columns:
-        clean_col = col.lower().strip()
+        clean_col = col.lower().strip().replace('_', '').replace(' ', '')
 
         new_name = ""
-        if clean_col in revcol_mapping:
-            new_name = (revcol_mapping[clean_col])
+        if clean_col in revcolumn_mapping:
+            new_name = (revcolumn_mapping[clean_col])
 
         if new_name != "" and new_name not in new_columns:
             new_columns.append(new_name)
@@ -38,140 +33,79 @@ def standardrize_columns(tdf,revcol_mapping):
             new_columns.append(clean_col)  # keep as is
         
     tdf.columns = new_columns
-
+    # print(new_columns)
     return tdf
 
-def read_dir(rootpath):
+def process_folder(folderpath):
+    db_result = {}
 
-    filepathlist = []
+    for item in os.listdir(folderpath):
+        item_path = os.path.join(folderpath, item)
+        
+        if os.path.isdir(item_path):
+            res = process_db_folder(item_path)
+            
+            print(f"{item}   {res}")
+            # print("final")
+            # print(res["Scoring"])
 
-    # OS.walk is fastest than pathlib or glob
-    for dirpath,dirname,filesname in os.walk(rootpath):
-       
-        for file in filesname:
-            if file.endswith(".csv"):
-                filepathlist.append(os.path.join(dirpath, file))
+            db_result[item] = res.get("Scoring","")
 
-    # print(filepathlist)
-    return filepathlist
-
-def check_file_columns(filenamelist):
-    
-    all_columns = []
-    summ_data = []
-    
-    # Extract all column sets    
-    all_columns = [set(pd.read_csv(f, nrows=0).columns) for f in filenamelist]
-    # all_columns.append(pd.read_csv(f, nrows=0).columns for f in filenamelist)
-
-    for f in filenamelist:
-        tdf = pd.read_csv(f, nrows=0)
-        cols = ", ".join(tdf.columns)
-        file_name = os.path.basename(f)
-        summ_data.append({
-            "file_name": file_name,
-            "columns": cols
-        })
-    
-    return summ_data
-   
-    # Find intersection across all sets
-    # common_to_all = reduce(set.intersection, all_columns)
-    # print(common_to_all)
-    # print(len(common_to_all)
-    # print(f"Columns in every file: {common_to_all}")
+    return db_result
 
 
-def read_files(filenamelist,rev_col_mapping):
-    datafrms = []
-    for file in filenamelist:
-        tmpdf = pd.read_csv(
-                            file,
-                            on_bad_lines='skip',   # skips corrupted rows
-                            quotechar='"',
-                            low_memory=False
-        )
-        tmpdf = standardrize_columns(tmpdf,rev_col_mapping)
-   
-        if 'uniq_id' in tmpdf.columns:
-            tmpdf['new_uniq_id'] = tmpdf['uniq_id'].replace('', pd.NA).fillna(tmpdf.get('fingerprint'))
-        else:
-            tmpdf['new_uniq_id'] = tmpdf.get('fingerprint')
+def process_db_folder(folderpath:str,basepath:str=None):
+    result_row = {}
 
-        tmpdf['source_file'] = os.path.basename(file)
-        datafrms.append(tmpdf)
+    # store base root path
+    basepath = folderpath if basepath == None else basepath
+    # print(f"{basepath}   {folderpath}")
 
-    combined_df = pd.concat(datafrms, ignore_index=True)
-    return combined_df
+    for item in os.listdir(folderpath):
+        item_path = os.path.join(folderpath, item)
 
+        # if folder go one more level down
+        if os.path.isdir(item_path):
+            subfolder_result = process_db_folder(item_path,basepath)
+            
+            # sub folfder will have blank result if not valid candidate to read
+            if subfolder_result:
+                result_row[item] = subfolder_result
 
-def data_cleaning(ipdf):
-    # print(ipdf.info())  
+        # read csv from scoring folder only
+        elif item.endswith(".csv") and "Scoring" in os.path.normpath(folderpath).split(os.sep):
+            try:
+                tdf = pd.read_csv(item_path)
+                tdf = standardrize_columns(tdf)
+                score = float(tdf["finalscore"].mean())
+                result_row[item] = round(score, 4)
 
-    # remove rows with null in key cols
-    ipdf.dropna(subset=['new_uniq_id'],inplace=True)
+            except Exception as e:
+                print(f"Error reading {item}: {e}")
 
-    #Remove any duplicates
-    ipdf.drop_duplicates(inplace=True)    
+    return result_row
 
-    # Clean wrong data-Either renove it or fix data like define rule based on columns
-    # for x in ipdf.index:
-    #     if ipdf.loc[x,"cp"] > 3:
-    #         ipdf.loc[x,'cp'] = 3
-    
-    return ipdf
+def newmain(root_path,outputfile,summaryfile):
+    final_output = process_folder(root_path)
 
+    with open(outputfile, "w") as f:
+        json.dump(final_output, f, indent=4)  # 'indent' makes it human-readable
 
-def data_analysis(ipfdf,rec_count,file_count,):
-     
-    # with open("C:/WorkSakshi/Python/data/healthOutput/corrfile.csv", "w") as f:
-    #     f.write(ipcldf.corr(numeric_only=True).to_string())
+    print(final_output)
 
-    file_summary = {}
-    file_summary['Total Files Processed'] = file_count
-    file_summary['Records Before Cleaning'] = rec_count
-    file_summary['Records After Cleaning'] = len(ipfdf)
-    
-    # file_summary['Average Patient Age'] = int(ipfdf['age'].mean())
-    # filtered_ds = ipfdf.query('(cp == 0 & exang == 1) or oldpeak > 2')
-    # file_summary['Persons with possibility of heart attack'] = len(filtered_ds)
+    rows = []
+    for dbname, modules in final_output.items():
+        for module, tables in modules.items():
+            for tbl, score in tables.items():
+                rows.append([dbname, module, tbl, score])
 
-    # # Example Aggregations
-    # numeric_cols = ipfdf.select_dtypes(include='number').columns
+    df = pd.DataFrame(rows, columns=["dbname", "module", "table", "score"])
 
-    # if len(numeric_cols) > 0:
-    #     file_summary['Aggregations'] = ipfdf[numeric_cols].agg(['sum', 'mean', 'count']).to_dict()
-
-    return file_summary
-
-def mainblk(rootpath,summaryfile,outputfile):
-    revcolumn_mapping = create_col_mapping(column_mapping)
-
-    files_to_scan = read_dir(rootpath)
-
-    cols_data = check_file_columns(files_to_scan)
-    my_df = pd.DataFrame(cols_data)
-    # Save to CSV
-    my_df.to_csv("C:/WorkSakshi/Python/data/dumpOutput/AllColsSumm.csv", index=False)
-
-    # unified Consolidated Data
-    mergeddf = read_files(files_to_scan,revcolumn_mapping)
-    merged_cnt = len(mergeddf)
-    
-    print(mergeddf.info()) 
-
-    finaldf = data_cleaning(mergeddf)
-    summary = data_analysis(finaldf,merged_cnt,len(files_to_scan))
-     # file containing all processed data
-    finaldf.to_csv(outputfile)
-
-    with open(summaryfile, "w") as f:
-        for key, value in summary.items():
-            f.write(f"{key}, {value}\n")
-
+    print(df)
 
 # Set path and file names
 rootfolderpath = "C:/WorkSakshi/Python/data/1"
 summaryfilename = "C:/WorkSakshi/Python/data/dumpOutput/summ.csv"
 consolidatedfilename = "C:/WorkSakshi/Python/data/dumpOutput/AllData.csv"
-mainblk(rootfolderpath,summaryfilename,consolidatedfilename)
+# mainblk(rootfolderpath,consolidatedfilename,summaryfilename)
+newmain(rootfolderpath,consolidatedfilename,summaryfilename)
